e0434462c0b82a6d3b27d02e678a825a4114636a
Adrien PEREA <adrien@perea.be>
2021-03-22 13:36:33 +0100

    projet version 0.1 premier commit depuis git

---
+++ SportClub/designedView.swift
@@ -0,0 +1,35 @@
+//
+//  designedView.swift
+//  SportClub
+//
+//  Created by Adrien PEREA on 14/03/2021.
+//
+
+import UIKit
+
+@IBDesignable
+class DesignableView: UIView {
+    @IBInspectable var shadowColor: UIColor = UIColor.clear {
+        didSet {
+            layer.shadowColor = shadowColor.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowRadius: CGFloat = 0 {
+        didSet {
+            layer.shadowRadius = shadowRadius
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: CGFloat = 0 {
+        didSet {
+            layer.shadowOpacity = Float(shadowOpacity)
+        }
+    }
+    
+    @IBInspectable var shadowOffsetY: CGFloat = 0 {
+        didSet {
+            layer.shadowOffset.height = shadowOffsetY
+        }
+    }
+}
